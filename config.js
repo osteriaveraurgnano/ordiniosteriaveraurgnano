a8e30825fd4e8febef6b75502e81ec40a6cd5d39
osteriaveraurgnano <osteriaveraurgnano@gmail.com>
2026-07-21 13:44:58 +0200

    Update config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAXpWgQHgx3hgj7AhEPbJd9OhRJ8L606cU",
+    authDomain: "comande-osteria-vera.firebaseapp.com",
+    projectId: "comande-osteria-vera",
+    storageBucket: "comande-osteria-vera.firebasestorage.app",
+    messagingSenderId: "128040702699",
+    appId: "1:128040702699:web:acad39f79c34257b16a4ae"
+};
